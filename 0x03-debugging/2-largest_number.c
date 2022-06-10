6a4dce34ad45dd8443f0a775898af72f01b53cc7
seblewengell <tseblewengle@yahoo.com>
2022-06-09 22:28:10 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,22 +8,20 @@
  *      *      * Return: largest number                                                                                                      
  *       *       */                                                                                                                           
                                                                                                                                       
-int largest_number(int a, int b, int c)                                                                                               
-{                                                                                                                                     
-	                int largest;                                                                                                        
-			                                                                                                                       
-			               if (a >= b && a >= c)                                                                                         
-					                       {                                                                                             
-								                        largest = a;                                                          
-											                 }                                                             
-				                      else if (b >= a && b >= c)                                                                            
-							                    {                                                                                     
-										                      largest = b;                                                  
-												                            }                                                     
-						                            else                                                                                          
-										                          {                                                                             
+int largest_number(int a, int b, int c)
+
+{                                                                                                                                   
+       	int largest;                                                                                                        
+			                                                                                                      
+      	if (a >= b && a >= c)                                                                              
+   	{                                                 								                        largest = a;              
+	}                                                         
+	else if (b >= a && b >= c)                                                                            
+	{                                                                                     
+		largest = b;                                                  
+	}                                                     
+	else                                                                                          
+	{                                                                             
 														                      largest = c;                                          
-																                         }                                             
-									                                                                                                                                          
-									                    return (largest);                                                                     
+																                         }                                             									                                                                                                                                          return (largest);               
 }        
