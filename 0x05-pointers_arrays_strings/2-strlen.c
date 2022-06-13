3cfda3681b28f4f0a50749aaf621bd769c8609a1
seblewengell <tseblewengle@yahoo.com>
2022-06-13 00:59:04 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+able file 15 lines (13 sloc) 229 Bytes
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
